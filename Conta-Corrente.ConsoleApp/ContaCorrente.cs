ea59bebb74957cd9c3416a6a5df842f1735f6902
Agatha Sates <satesagatha@gmail.com>
2025-04-03 01:12:25 -0300

    refactor: Melhorado estrutura do codigo

--- Conta-Corrente.ConsoleApp/ContaCorrente.cs
+++ Conta-Corrente.ConsoleApp/ContaCorrente.cs
@@ -4,9 +4,9 @@ class ContaCorrente
 {
     public int iD;
     public decimal saldo;
-    public decimal lis = 200; //limite Saque
+    public decimal lis = 0; //limite Saque
     public decimal LimiteTotalParaSaque;
-    public string[] transacoes = new string[100];
+    public string[] transacoes = new string[10];
     public int transacao;
 
     public ContaCorrente(decimal saldo)
@@ -17,17 +17,61 @@ class ContaCorrente
         lis = lis;
     }
 
-    public decimal LimitedeSaque() 
-    {
-        LimiteTotalParaSaque = saldo + lis;
-        return LimiteTotalParaSaque;
-    }
-
     public void Saque(decimal valor)
     {
         valor = ExcedeuoLimitedeSaque(valor);
         saldo -= valor;
-        transacoes[transacao++] = $" Saque de $ {valor}";
+        transacoes[transacao++] = $"Saque de $ {valor}";
+    }
+
+    public void Deposito(decimal valor)
+    {
+        saldo += valor;
+        transacoes[transacao++] = $"Depósito de $ {valor}";
+    }
+
+    public void TransferirParaOutraConta(ContaCorrente conta, decimal valor)
+    {
+        conta.saldo += valor;
+        conta.transacoes[transacao++] = $"Recebido $ {valor} da conta {iD}";
+        saldo -= valor;
+        transacoes[transacao++] = $"Transferido $ {valor} para a conta {conta.iD}";
+    }
+
+    public void ConsultarSaldo()
+    {
+        Console.WriteLine($"---------Conta #{iD}--------");
+        Console.WriteLine($"Seu saldo atual é: $ {saldo}");
+    }
+
+    public void Extrato()
+    {
+        Console.WriteLine($"-------- Extrato da Conta #{iD} --------");
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("             Transações");
+        Console.WriteLine("---------------------------------------");
+        MostrarTransacoes();
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine($"Saldo atual: {saldo}");
+        Console.WriteLine("---------------------------------------");
+    }
+
+    private void MostrarTransacoes()
+    {
+        if (transacoes[0] == null)
+            Console.WriteLine("Não foram registradas transações");
+        else
+            foreach (string transacao in transacoes)
+            {
+                if (transacao != null)
+                    Console.WriteLine(transacao);
+            }
+    }
+
+    public decimal LimitedeSaque()
+    {
+        LimiteTotalParaSaque = saldo + lis;
+        return LimiteTotalParaSaque;
     }
 
     public decimal ExcedeuoLimitedeSaque(decimal valor)
@@ -38,39 +82,6 @@ class ContaCorrente
             Console.Write("digite um valor novamente:");
             valor = Convert.ToDecimal(Console.ReadLine());
         }
-
         return valor;
-    }
-
-    public void Deposito(decimal valor)
-    {
-        saldo += valor;
-        transacoes[transacao++] = $" Déposito de $ {valor}";
-    }
-
-    public void ConsultarSaldo()
-    {
-        Console.WriteLine($"Seu saldo atual é: $ {saldo}");
-    }
-
-    public void TransferirParaOutraConta(ContaCorrente conta, decimal valor)
-    {
-        conta.saldo += valor;
-        conta.transacoes[transacao++] = $"Recebeu {valor} de {iD}";
-        saldo -= valor;
-        transacoes[transacao++] = $"Transferido$ {valor} da conta {iD} para {conta.iD}";
-       
-    }
-
-    public void Extrato()
-    {
-        Console.WriteLine("-------Tansações feitas----");
-        
-        foreach (string transacao in transacoes) 
-        {
-            if (transacao != null)          
-                Console.WriteLine(transacao);
-        }
-        Console.WriteLine($"seu saldo: {saldo}");
     }
 }
