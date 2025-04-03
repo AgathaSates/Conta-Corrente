bc4750724e40cd17f10501bec645392991208d5c
Agatha Sates <satesagatha@gmail.com>
2025-04-02 23:03:12 -0300

    feat: Criado objeto criar ID

---
+++ Conta-Corrente.ConsoleApp/GerarID.cs
@@ -0,0 +1,47 @@
+﻿namespace ContaCorrente.ConsoleApp;
+
+class GerarID
+{
+    public int iD;
+    public int[] listaIDs = new int[100];
+    public int iDs;
+
+    public int ObterID() 
+    {
+        GerarIDAleatorio();
+        IDJaExiste();
+        AdicionarnaListadeID();
+
+        return iD;
+    }
+
+    public void GerarIDAleatorio()
+    {
+        Random geraID = new Random();
+        iD = geraID.Next(100000);
+        
+    }
+
+    public void IDJaExiste()
+    {
+        while (true)
+        {
+            if (listaIDs.Contains(iD))
+            {
+               GerarIDAleatorio();
+            }
+            else
+                break;
+        }
+    }
+
+    public void AdicionarnaListadeID()
+    {
+        listaIDs[iDs++] = iD;
+    }
+
+    public override string ToString()
+    {
+        return iD.ToString();
+    }
+}
