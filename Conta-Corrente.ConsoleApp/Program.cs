8cdb6d52cc2ae268382de0e89679a0f06957a0cf
Agatha Sates <satesagatha@gmail.com>
2025-04-03 01:14:21 -0300

    feat: Imprementado transações

--- Conta-Corrente.ConsoleApp/Program.cs
+++ Conta-Corrente.ConsoleApp/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Conta_Corrente.ConsoleApp
+﻿namespace ContaCorrente.ConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        ContaCorrente conta1 = new(1000);
+        ContaCorrente conta2 = new(300);
+
+        conta1.Saque(200);
+        conta1.Deposito(300);
+        conta1.Deposito(500);
+        conta1.Saque(200);
+        conta1.TransferirParaOutraConta(conta2, 400);
+
+        conta1.Extrato();
+        Console.WriteLine();
+        conta2.Extrato();        
     }
 }
